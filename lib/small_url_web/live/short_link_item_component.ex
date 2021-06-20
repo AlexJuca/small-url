54c5dc03c6b391999b7d053b9390dfc761d32b2c
alexjuca <alexandre.juca@nextbss.co.ao>
2021-06-20 12:45:38 +0100

    implement short link component

--- lib/small_url_web/live/short_link_item_component.ex
+++ lib/small_url_web/live/short_link_item_component.ex
@@ -5,13 +5,15 @@ defmodule SmallUrlWeb.ShortLinkItemComponent do
     def render(assigns) do
         ~L"""
         <div class="space-y-4">
-            <div class="h-14 w-full block">
-                <span class="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
-                <span class="ml-6 inline-flex"><p class="">https://www.google.com</p></span>
-                <span class="ml-40 inline-flex"><p class="font-bold text-indigo-500">https://www.sm.ly/Hr4818</p></span>
-                <span class="ml-4 inline-flex"><button class="focus:outline-none p-2 rounded-md bg-gray-200 text-indigo hover:shadow-md" type="submit">Copy</button></span>
-                <div class="w-full mt-2 h-0.5 bg-gray-100"></div>
-            </div>
+            <%= for link <- @short_links do %>
+                <div class="h-14 w-full block">
+                    <span class="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
+                    <span class="w-8 ml-6 inline-flex"><p class=""><%= "#{String.slice(link.url, 0, 30)}..." %></p></span>
+                    <span class="w-20 ml-80 inline-flex"><p class="font-bold text-indigo-500"><%= link(link.key, to: "#{SmallUrlWeb.Endpoint.url()}/#{link.key}") %></p></span>
+                    <span class="ml-4 inline-flex"><button class="focus:outline-none p-2 rounded-md bg-gray-200 text-indigo hover:shadow-md" type="submit">Copy</button></span>
+                    <div class="w-full mt-2 h-0.5 bg-gray-100"></div>
+                </div>
+            <% end %>
         </div>
         """
     end
