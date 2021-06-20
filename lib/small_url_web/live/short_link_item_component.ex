ec6a63d772b846cdb9e9069db9d9eebad8b9755e
alexjuca <alexandre.juca@nextbss.co.ao>
2021-06-20 17:12:39 +0100

    handle qr action

--- lib/small_url_web/live/short_link_item_component.ex
+++ lib/small_url_web/live/short_link_item_component.ex
@@ -3,19 +3,36 @@ defmodule SmallUrlWeb.ShortLinkItemComponent do
 
     @impl true
     def render(assigns) do
+        IO.inspect assigns
+
         ~L"""
         <div class="space-y-4">
             <%= for link <- @short_links do %>
                 <div class="h-14 w-full block">
                     <span class="relative inline-flex rounded-full h-2 w-2 bg-green-500"></span>
                     <span class="w-8 ml-6 inline-flex"><p class=""><%= "#{String.slice(link.url, 0, 30)}..." %></p></span>
-                    <span class="w-20 ml-80 inline-flex"><p class="font-bold text-indigo-500"><%= link(link.key, to: "#{SmallUrlWeb.Endpoint.url()}/#{link.key}") %></p></span>
+                    <span class="w-20 ml-80 inline-flex"><p class="font-bold text-indigo-500"><%= link(link.key, to: "#{SmallUrlWeb.Endpoint.url()}/url/#{link.key}") %></p></span>
                     <span class="ml-4 inline-flex"><button class="focus:outline-none p-2 rounded-md bg-gray-200 text-indigo hover:shadow-md" type="submit">Copy</button></span>
-                    <span class="ml-4 inline-flex"><i class="ri-qr-code-line cursor-pointer"></i></span>
+                    <span class="ml-4 inline-flex">
+                        <%= live_patch to: Routes.page_path(@socket, :qr),
+                            class: "button button-outlined-gray whitespace-nowrap" 
+                        do %>
+                            <i class="ri-qr-code-line cursor-pointer"></i>
+                        <% end %>
+                    </span>
                     <div class="w-full mt-2 h-0.5 bg-gray-100"></div>
                 </div>
             <% end %>
         </div>
+        
+        <%= if @live_action == :qr do %>
+        <%= live_modal SmallUrlWeb.QrUrlComponent,
+                id: :close_session_modal,
+                modal_class: "w-full max-w-xl",
+                return_to: Routes.page_path(@socket, :index)
+                %>
+        <% end %>
+       
         """
     end
 end
