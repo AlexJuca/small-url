bdaf0f4ae98ebcb386fca6bb0571679fe2ac92fa
alexjuca <alexandre.juca@nextbss.co.ao>
2021-06-20 13:51:39 +0100

    add qr-code button

--- lib/small_url_web/live/short_link_item_component.ex
+++ lib/small_url_web/live/short_link_item_component.ex
@@ -11,6 +11,7 @@ defmodule SmallUrlWeb.ShortLinkItemComponent do
                     <span class="w-8 ml-6 inline-flex"><p class=""><%= "#{String.slice(link.url, 0, 30)}..." %></p></span>
                     <span class="w-20 ml-80 inline-flex"><p class="font-bold text-indigo-500"><%= link(link.key, to: "#{SmallUrlWeb.Endpoint.url()}/#{link.key}") %></p></span>
                     <span class="ml-4 inline-flex"><button class="focus:outline-none p-2 rounded-md bg-gray-200 text-indigo hover:shadow-md" type="submit">Copy</button></span>
+                    <span class="ml-4 inline-flex"><i class="ri-qr-code-line cursor-pointer"></i></span>
                     <div class="w-full mt-2 h-0.5 bg-gray-100"></div>
                 </div>
             <% end %>
