4fedb92210af5b898f42d07bc9155ebde89a226a
alexjuca <alexandre.juca@nextbss.co.ao>
2021-06-20 13:49:55 +0100

    import remix icons

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,7 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import topbar from "topbar"
 import {LiveSocket} from "phoenix_live_view"
+import 'remixicon/fonts/remixicon.css'
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
 let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
