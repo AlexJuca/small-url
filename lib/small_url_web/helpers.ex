0c47d055a54dabda883f27ea995ef6c6f2920db2
alexjuca <alexandre.juca@nextbss.co.ao>
2021-06-20 17:09:00 +0100

    implement our custom helper

---
+++ lib/small_url_web/helpers.ex
@@ -0,0 +1,34 @@
+defmodule SmallUrlWeb.Helpers do
+    import Phoenix.LiveView.Helpers
+    import Phoenix.HTML.Tag
+
+    @doc """
+  Renders a component inside the `Livebook.ModalComponent` component.
+
+  The rendered modal receives a `:return_to` option to properly update
+  the URL when the modal is closed.
+  """
+  def live_modal(component, opts) do
+    path = Keyword.fetch!(opts, :return_to)
+    modal_class = Keyword.get(opts, :modal_class)
+
+    modal_opts = [
+      id: :modal,
+      return_to: path,
+      modal_class: modal_class,
+      component: component,
+      opts: opts
+    ]
+
+    live_component(SmallUrlWeb.ModalComponent, modal_opts)
+  end
+
+  @doc """
+  Returns [Remix](https://remixicon.com) icon tag.
+  """
+  def remix_icon(name, attrs \\ []) do
+    icon_class = "ri-#{name}"
+    attrs = Keyword.update(attrs, :class, icon_class, fn class -> "#{icon_class} #{class}" end)
+    content_tag(:i, "", attrs)
+  end
+end
