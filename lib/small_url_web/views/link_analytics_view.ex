7940620dadb048f81677e1f5bf03e72eaf424cfc
alexjuca <alexandre.juca@nextbss.co.ao>
2021-06-25 07:14:35 +0100

    implement analytics view

--- lib/small_url_web/views/link_analytics_view.ex
+++ lib/small_url_web/views/link_analytics_view.ex
@@ -1,10 +1,10 @@
 defmodule SmallUrlWeb.LinkAnalyticsView do
-    use SmallUrlWeb, :view
+  use SmallUrlWeb, :view
 
-    def render("analytics.json", %{link_info: link}) do
-        %{
-            :clicks => link.clicks,
-            :last_click_date => link.last_click_date
-        }
-    end
+  def render("analytics.json", %{link_info: link}) do
+    %{
+      :clicks => link.clicks,
+      :last_click_date => link.last_click_date
+    }
+  end
 end
