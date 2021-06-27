cc2db02f48c13cac98e633378576173ad18d2c62
alexjuca <alexandre.juca@nextbss.co.ao>
2021-06-27 20:29:13 +0100

    implement feature to share link via facebook

--- assets/js/shortlink/index.js
+++ assets/js/shortlink/index.js
@@ -1,4 +1,5 @@
 import copy from 'copy-to-clipboard';
+import shareFacebook from 'share-facebook';
 
 const ShortLink = {
     mounted() {
@@ -22,13 +23,60 @@ function handleDocumentMouseDown(hook, event) {
         if (event.target.closest(`[data-element="copy-url-to-clipboard"]`)) {
             copyURLToClipboard(hook, event);
         }
+
+        if (event.target.closest(`[data-element="share-on-facebook"]`)) {
+            shareToFacebook(hook, event);
+        }
     }
 
     return;
 }
 
+
+function shareToFacebook(hook, event) {
+    const item = event.target;
+    console.log(item);
+
+    if (item) {
+        const url = item.getAttribute("data-element-id");
+        console.log(url);
+        if (url) {
+            const facebookShareLink = shareFacebook({
+                quote: 'Checkout my awesome shortlink',
+                href: url,
+                redirect_uri: url,
+                app_id: '163966962458513'
+            });
+            const newTab = window.open(facebookShareLink);
+        }
+    }
+
+
+}
+
+function showToast(message) {
+    const copyAlert = document.getElementById("copy-toast");
+    copyAlert.innerHTML = `<div class="z-40 md:z-50 p-2 bg-indigo-800 items-center text-indigo-100 shadow-xl leading-none lg:rounded-full flex lg:inline-flex" role="alert">
+        <span class="flex rounded-full bg-green-500 uppercase px-2 py-1 text-xs font-bold mr-3"><i class="text-white ri-check-line"></i></span>
+        <span class="text-white font-semibold mr-2 text-left flex-auto">${message}</span>
+    </div>`;
+    copyAlert.classList.remove("invisible");
+    copyAlert.classList.add("transition", "duration-2000", "ease-in-out", "opacity-0");
+
+    window.setTimeout(function () {
+        copyAlert.innerHTML = "";
+        copyAlert.classList.add("invisible");
+        copyAlert.classList.remove("transition", "duration-2000", "ease-in-out", "opacity-0");
+    }, 2000);
+}
+
 function copyURLToClipboard(hook, event) {
     const shortLinkItem = event.target;
+
+    if (document.getElementById("copy-toast")) {
+        showToast("Copied to clipboard");
+    }
+
     if (shortLinkItem) {
         var URL = shortLinkItem.getAttribute("data-element-id");
         if (URL) {
