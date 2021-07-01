9f7185032260bfc54e0d9901a8d17156bdb4e74c
alexjuca <alexandre.juca@nextbss.co.ao>
2021-07-01 21:29:57 +0100

    remove unused functions

--- lib/small_url_web/controllers/url_controller.ex
+++ lib/small_url_web/controllers/url_controller.ex
@@ -5,37 +5,6 @@ defmodule SmallUrlWeb.UrlController do
   alias SmallUrl.Links.ShortLinks
   alias SmallUrl.Repo
 
-  @url %{}
-
-  def new(conn, params) do
-    id = Ksuid.generate() |> String.slice(4, 7)
-    @url = Map.put_new(@url, id, params[:url])
-    IO.inspect(@url)
-    IO.inspect(id)
-
-    conn
-    |> put_resp_content_type("application/json")
-    |> send_resp(201, id)
-    |> halt()
-  end
-
-  def get(conn, %{"id" => id} = params) do
-    url = Map.get(@url, id)
-    IO.inspect(@url)
-
-    case url do
-      nil ->
-        conn
-        |> send_resp(404, "Not found")
-        |> halt()
-
-      id ->
-        conn
-        |> send_resp(200, id)
-        |> halt()
-    end
-  end
-
   def redirect_to_original_url(conn, %{"key" => key} = params) do
     shortlink = Links.get_short_links_by_key(key)
 
