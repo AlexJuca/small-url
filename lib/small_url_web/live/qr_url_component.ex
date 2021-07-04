a408798b187c7c31e0baa61bdeeaf970931c588e
alexjuca <alexandre.juca@nextbss.co.ao>
2021-07-04 20:01:53 +0100

    Fix: unexpected beginning of EEx tag =

--- lib/small_url_web/live/qr_url_component.ex
+++ lib/small_url_web/live/qr_url_component.ex
@@ -17,7 +17,7 @@ defmodule SmallUrlWeb.QrUrlComponent do
                     <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
                     Cancel
                     </button>
-                <%= end %>
+                <% end %>
             </div>
         </div>
     </div>
